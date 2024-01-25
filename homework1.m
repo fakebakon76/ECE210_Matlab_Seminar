db3499248c42b28bdefe77b4ae7a69d605219d09
noamschuck <noam.schuck@gmail.com>
2024-01-25 17:39:18 -0500

    Homework 1 Finished

---
+++ homework1.m
@@ -0,0 +1,29 @@
+% homework1.m
+% Copywrite (C) 2024 Noam Schuck <noam.schuck@gmail.com>
+% ECE210 Matlab Seminar Homework 1
+
+% Question 1
+u = [11 13 17];
+v = [-1; -1; -1];
+A = [-u; 2 * u; 7 * u];
+B = [A' v];
+
+disp(A);
+disp(B);
+
+% Question 2
+c = exp(pi * 1j * 4);
+d = sqrt(1j);
+l = floor((8.418 * 10^6)^(1 / 2.1));
+k = floor(100 * log(2)) + ceil(exp(7.5858));
+
+% Question 3
+
+A = [1 -11 -3
+     1 1   0
+     2 5   1
+    ];
+
+b = [-37; -1; 10];
+
+x = mldivide(A, b);
